db6a85f0861942011935bf0101897bb9447c422c
MadelenVillaca <madelenvillaca@gmail.com>
2022-05-17 11:15:51 -0400

    añadiendo lucessemidirectas

---
+++ src/app/luces-semidirectas/luces-semidirectas.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-luces-semidirectas',
+  templateUrl: './luces-semidirectas.component.html',
+  styleUrls: ['./luces-semidirectas.component.css']
+  
+})
+export class LucesSemidirectasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
