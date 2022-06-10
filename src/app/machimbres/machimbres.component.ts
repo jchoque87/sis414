da4f6758c1824be7d21be34d4bcd6e7f1250dfa2
Usuario1 <hahhedadey@gmail.com>
2022-06-10 09:58:44 -0400

    añadiendo machimbres

---
+++ src/app/machimbres/machimbres.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-machimbres',
+  templateUrl: './machimbres.component.html',
+  styleUrls: ['./machimbres.component.css']
+})
+export class MachimbresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
