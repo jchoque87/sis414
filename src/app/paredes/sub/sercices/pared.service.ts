916de34b1cffc2d34139e516e579bcb7fa1d389c
ErickTorrez <gabo1cnc@gmail.com>
2022-07-06 17:43:14 -0400

    altualizando pre final pagina

--- src/app/paredes/sub/sercices/pared.service.ts
+++ src/app/paredes/sub/sercices/pared.service.ts
@@ -11,16 +11,17 @@ export class ParedService {
 
   constructor(private http: HttpClient) { }
 
-  getUsers() {
-    return this.http.get<any>(this.configUrl+"/users.json");
-  }
+  getUsers(){
+    console.log(this.configUrl+"/user.json");
+    return this.http.get<any>(this.configUrl+"/user.json");
 
+  }
   addUser(data:any){
-    return this.http.post<any>(this.configUrl+"/users.json",data);
+    return this.http.post<any>(this.configUrl+"/user.json",data);
   }
 
   deleteUser(key:any){
-    let url= `${this.configUrl}/users/${key}.json`;
+    let url= `${this.configUrl}/user/${key}.json`;
     return this.http.delete<any>(url).pipe(map(res =>{
       console.log(url);
       console.log(res);
